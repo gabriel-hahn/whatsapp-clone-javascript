c1c79cea5831b91613ee7895819d01d01327d057
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-09-25 23:54:00 -0300

    Some functions to get value from fields

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -49,6 +49,20 @@ class WhatsAppController {
         Element.prototype.hasClass = function (name) {
             return this.classList.contains(name);
         }
+
+        HTMLFormElement.prototype.getForm = function () {
+            return new FormData(this);
+        }
+
+        HTMLFormElement.prototype.toJSON = function () {
+            let json = {};
+
+            this.getForm().forEach((value, key) => {
+                json[key] = value;
+            });
+
+            return json;
+        }
     }
 
     loadElements() {
@@ -83,6 +97,26 @@ class WhatsAppController {
 
         this.el.btnClosePanelAddContact.on('click', e => {
             this.el.panelAddContact.removeClass('open');
+        });
+
+        this.el.photoContainerEditProfile.on('click', e => {
+            this.el.inputProfilePhoto.click();
+        });
+
+        this.el.inputNamePanelEditProfile.on('keypress', e => {
+            if (e.key === 'Enter') {
+                e.preventDefault();
+                this.el.btnSavePanelEditProfile.click();
+            }
+        });
+
+        this.el.btnSavePanelEditProfile.on('click', e => {
+
+        });
+
+        this.el.formPanelAddContact.on('submit', e => {
+            e.preventDefault();
+            let formData = new FormData(this.el.formPanelAddContact);
         });
     }
 
