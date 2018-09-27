49d8248daae5bf940580218dbdbe92d6013d7fcf
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-09-26 23:03:37 -0300

    Menu events

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -135,20 +135,62 @@ class WhatsAppController {
         });
 
         this.el.btnAttachPhoto.on('click', e => {
+            this.el.inputPhoto.click();
+        });
 
+        this.el.inputPhoto.on('change', e => {
+            [...this.el.inputPhoto.files].forEach(file => {
+                console.log(file);
+            });
         });
 
         this.el.btnAttachCamera.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelCamera.addClass('open');
+            this.el.panelCamera.css({
+                height: 'calc(100% - 120px)'
+            });
+        });
+
+        this.el.btnClosePanelCamera.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+        });
+
+        this.el.btnTakePicture.on('click', e => {
 
         });
 
         this.el.btnAttachDocument.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelDocumentPreview.addClass('open');
+            this.el.panelDocumentPreview.css({
+                height: 'calc(100% - 120px)'
+            });
+        });
+
+        this.el.btnClosePanelDocumentPreview.on('click', e => {
+            this.closeAllMainPanel();
+            this.el.panelMessagesContainer.show();
+        });
+
+        this.el.btnSendDocument.on('click', e => {
 
         });
 
         this.el.btnAttachContact.on('click', e => {
-
+            this.el.modalContacts.show();
         });
+
+        this.el.btnCloseModalContacts.on('click', e => {
+            this.el.modalContacts.hide();
+        });
+    }
+
+    closeAllMainPanel() {
+        this.el.panelMessagesContainer.hide();
+        this.el.panelDocumentPreview.removeClass('open');
+        this.el.panelCamera.removeClass('open');
     }
 
     closeMenuAttach(e) {
