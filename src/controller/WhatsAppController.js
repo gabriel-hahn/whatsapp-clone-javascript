3e37a7b5e7cfd2b51128f68af37112b475f797a4
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-09-27 23:08:51 -0300

    BtnMicrophone events and time format

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -185,6 +185,33 @@ class WhatsAppController {
         this.el.btnCloseModalContacts.on('click', e => {
             this.el.modalContacts.hide();
         });
+
+        this.el.btnSendMicrophone.on('click', e => {
+            this.el.recordMicrophone.show();
+            this.el.btnSendMicrophone.hide();
+            this.startRecordMicrophoneTime();
+        });
+
+        this.el.btnCancelMicrophone.on('click', e => {
+            this.closeRecordMicrophone();
+        });
+
+        this.el.btnFinishMicrophone.on('click', e => {
+            this.closeRecordMicrophone();
+        });
+    }
+
+    startRecordMicrophoneTime() {
+        let start = Date.now();
+        this._recordMicrophoneInterval = setInterval(() => {
+            this.el.recordMicrophoneTimer.innerHTML = Format.toTime(Date.now() - start);
+        }, 100);
+    }
+
+    closeRecordMicrophone() {
+        this.el.recordMicrophone.hide();
+        this.el.btnSendMicrophone.show();
+        clearInterval(this._recordMicrophoneInterval);
     }
 
     closeAllMainPanel() {
--- src/util/Format.js
+++ src/util/Format.js
@@ -1,7 +1,21 @@
 class Format {
+
     static getCamelCase(text) {
         let div = document.createElement('div');
         div.innerHTML = `<div data-${text}="id"></div>`;
         return Object.keys(div.firstChild.dataset)[0];
+    }
+
+    static toTime(duration) {
+        let seconds = parseInt((duration / 1000) % 60);
+        let minutes = parseInt((duration / (1000 * 60)) % 60);
+        let hours = parseInt((duration / (1000 * 60 * 60)) % 24);
+
+        if (hours > 0) {
+            return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+        }
+        else {
+            return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+        }
     }
 }
